34b56bbfeedb9d06b6cb5a1b0f8d187a0567e7b4
Megastary <megastary@houby-studio.eu>
2019-09-08 21:54:52 +0200

    Fixed typo and hopefully added catch for order when no stock left.

--- routes/api/keypadorder.js
+++ routes/api/keypadorder.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var mailer = require('../../functions/sendMail');
 var config = require('../../config/config');
 var User = require('../../models/user');
 var Order = require('../../models/order');
@@ -29,7 +31,7 @@ router.post('/', function(req, res, next) {
 
         // Get product
         Product.aggregate([
-            { $match: { 'keypadId': Number(req.body.keypadId) } },
+            { $match: { 'keypadId': Number(req.body.product) } },
             { $lookup: { from: 'deliveries', localField: '_id', foreignField: 'productId', as: 'stock'} },
             { $project: {
               keypadId: "$keypadId",
@@ -49,10 +51,15 @@ router.post('/', function(req, res, next) {
                 res.render('error');
                 return;
                 }
-
+                if (!product[0].stock[0]) {
+                    res.status(err.status || 500);
+                    res.render('error');
+                    return;
+                }
                 newOrder.deliveryId = product[0].stock[0]._id;
+                var newAmount = product[0].stock[0].amount_left--;
 
-                Delivery.findByIdAndUpdate(product[0].stock[0]._id, {amount_left: product[0].stock[0].amount_left--}, function (err, delivery) {
+                Delivery.findByIdAndUpdate(newOrder.deliveryId, { amount_left: newAmount }, function (err, delivery) {
                     if (err) {
                         res.status(err.status || 500);
                         res.render('error');
@@ -77,51 +84,6 @@ router.post('/', function(req, res, next) {
         );
     });
     res.status(200);
-
-    /*
-    
-  
-    Delivery.findOne({ '_id': req.body.product_id}, function(err,obj) {
-        if (err) {
-          var alert = { type: 'danger', component: 'db', message: err.message, danger: 1};
-          req.session.alert = alert;
-          res.redirect('/shop');
-          return;
-        }
-        obj.amount_left--;
-        obj.save(function (err) {
-            if (err) {
-                var alert = { type: 'danger', component: 'db', message: err.message, danger: 1};
-                req.session.alert = alert;
-                res.redirect('/shop');
-                var subject = `Nepodařilo se zapsat změny do databáze!`;
-                var body = `<h1>Chyba při zapisování do databáze při nákupu!</h1><p>Pokus o snížení skladové zásoby skončil chybou. Zkontrolujte konzistenci databáze!</p><p>Chyba: ${err.message}</p>`;
-                mailer.sendMail('system', subject, body);
-                return;
-            }
-            newOrder.save(function(err) {
-                if (err) {
-                    var alert = { type: 'danger', component: 'db', message: err.message, danger: 1};
-                    req.session.alert = alert;
-                    res.redirect('/shop');
-                    var subject = `Nepodařilo se zapsat změny do databáze!`;
-                    var body = `<h1>Chyba při zapisování do databáze při nákupu!</h1><p>Pokus o vytvoření záznamu nákupu skončil chybou. Zkontrolujte konzistenci databáze!</p><p>Chyba: ${err.message}</p>`;
-                    mailer.sendMail('system', subject, body);
-                    return;
-                }
-                var alert = { type: 'success', message: `Zakoupili jste ${req.body.display_name} za ${req.body.product_price}Kč.`, success: 1};
-                req.session.alert = alert;
-                res.redirect('/shop');
-                if (req.user.sendMailOnEshopPurchase) {
-                  var subject = `Děkujeme za nákup!`;
-                  var body = `<h1>Výborná volba!</h1><p>Tímto jste si udělali radost:</p><img width="135" height="240" style="width: auto; height: 10rem;" alt="Obrázek zakoupeného produktu" src="cid:image@prdelka.eu"/><p>Název: ${req.body.display_name}<br>Cena: ${req.body.product_price}Kč<br>Kdy: ${moment().format('LLLL')}</p><p>Přijďte zas!</p>`;
-                  mailer.sendMail(req.user.email, subject, body, req.body.image_path);
-                }
-                return;
-            });
-        });
-    });
-    */
 });
 
 module.exports = router;
