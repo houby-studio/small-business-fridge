a70ea6c7b17a019432967bbc160310f5c645728d
Jakub Šindelář <25355950+megastary@users.noreply.github.com>
2019-11-15 21:29:46 +0100

    Added GET customer email to test API

--- routes/api/customerName.js
+++ routes/api/customerName.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var router = express.Router();
-/* var moment = require('moment');
-var mailer = require('../../functions/sendMail');
-var config = require('../../config/config');
+// var moment = require('moment');
+// var mailer = require('../../functions/sendMail');
+// var config = require('../../config/config');
 var User = require('../../models/user');
-var Order = require('../../models/order');
-var Product = require('../../models/product');
-var Delivery = require('../../models/delivery'); */
+// var Order = require('../../models/order');
+// var Product = require('../../models/product');
+// var Delivery = require('../../models/delivery');
 
 
 router.get('/', function (req,res, next) {
 
     console.log('Yay a request!');
-    
+    User.findOne({ keypadId: req.body.customer }, function (err, user) {
+        if (err) {
+            res.status(err.status || 500);
+            res.render('error');
+            return;
+        }
+    res.render(user.email);
+    });
+
 });
 
 module.exports = router;
