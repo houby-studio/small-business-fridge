63fa8226f000616aeba820881e20c4a05f519a53
Jakub Šindelář <25355950+megastary@users.noreply.github.com>
2019-11-17 21:49:53 +0100

    Fix to expected header

--- tests/test.js
+++ tests/test.js
@@ -37,7 +37,7 @@ describe("Small Business Fridge", () => {
                 .set('Accept', 'application/problem+json')
                 .end((err, res) => {
                     res.should.have.status(400);
-                    res.should.be.json;
+                    res.should.have.header('content-type', 'application/problem+json');
                     done();
                 });
         });
