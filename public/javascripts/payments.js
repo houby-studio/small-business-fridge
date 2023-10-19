cb9961f6ac3ae837eadc988e63cd330406f16754
Jakub Šindelář <megastary@proton.me>
2023-10-19 16:45:24 +0200

    Better ordering and greyed out old records

--- public/javascripts/payments.js
+++ public/javascripts/payments.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
   $('#table-invoices').DataTable({
     columnDefs: [{ type: 'natural', targets: [2, 3] }],
     language: {
-      url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Czech.json'
+      url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Czech.json',
+      searchPlaceholder: 'Hledaný výraz'
     },
+    order: [[4, 'asc']],
     lengthMenu: [
       [10, 25, 50, -1],
       [10, 25, 50, 'Vše']
