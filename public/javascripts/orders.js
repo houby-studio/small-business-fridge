7e384cf1b42dfce29a4ab35d691a13b0278d78ea
Jakub Šindelář <megastary@proton.me>
2023-10-19 16:46:47 +0200

    Fix no orders on orders page

--- public/javascripts/orders.js
+++ public/javascripts/orders.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
   $('#table-orders').DataTable({
     columnDefs: [{ type: 'natural', targets: 2 }],
     language: {
-      url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/cs.json'
+      url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/cs.json',
+      searchPlaceholder: 'Hledaný výraz'
     },
     lengthMenu: [
       [10, 25, 50, -1],
