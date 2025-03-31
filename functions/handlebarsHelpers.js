316098ca097b0eb3996dccebc482fe4bc9c8881b
Jakub Šindelář <megastary@proton.me>
2025-03-31 09:24:40 +0200

    Add feedback url helper

--- functions/handlebarsHelpers.js
+++ functions/handlebarsHelpers.js
@@ -12,6 +12,9 @@ const helpers = {
       return options.fn(this)
     }
     return options.inverse(this)
+  },
+  feedbackUrl: function () {
+    return process.env.FEEDBACK_URL ? process.env.FEEDBACK_URL : null
   }
 }
 
