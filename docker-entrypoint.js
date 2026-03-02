bc99c5328914ea6c35b2e663aaae1d65a7059fbe
Jakub šindelář <jakub.sindelar@mountfield.cz>
2026-03-02 08:18:09 +0100

    fix: add DB migrate ENV to compose and env.example

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -4,7 +4,7 @@ import { execSync } from 'node:child_process'
  * Run migrations before starting the server
  * when the MIGRATE environment variable is set.
  */
-if (process.env.MIGRATE === 'true') {
+if (process.env.DB_MIGRATE === 'true') {
   console.log('Running migrations...')
   execSync('node ace migration:run --force', { stdio: 'inherit' })
 }
